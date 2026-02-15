b7af2c4a986a4766cf566039d3d4b31cf64f1563
Thomas Lucking <telucking@gmail.com>
2026-02-15 20:11:56 +0100

    feat: added basic fetching capabilities using useFetch from VueUse

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<script setup></script>
+<script setup>
+import { useFetch } from '@vueuse/core'
+
+const url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=auto'
+
+
+const { data, isFetching, error } = useFetch(url).json()
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="p-6">
+    <div v-if="error" class="text-red-500">
+      Error: {{ error }}
+    </div>
+
+    <div v-else-if="isFetching">
+      <p class="animate-pulse">Fetching</p>
+    </div>
+
+    <div v-else-if="data">
+      <h1 class="text-2xl font-bold">{{ data.timezone }}</h1>
+      <p class="text-4xl">{{ data.current.temperature_2m }}°C</p>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
