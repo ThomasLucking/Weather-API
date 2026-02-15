da5cab923ff81001f61fc95278cc66b39a242a36
Thomas Lucking <telucking@gmail.com>
2026-02-15 20:15:09 +0100

    feat: created component structure

---
+++ src/components/navbar.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('New Project Who Dis?')
+</script>
+
+<template>
+  <div class="p-8">
+    <h1 class="text-2xl font-bold">{{ message }}</h1>
+    
+    </div>
+</template>
+
+<style scoped></style>
